fca84b266d1297dc0f5dce3cfa310904737e2cb0
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-06-16 11:43:05 -0500

    added end line decriment

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var friends = ['Brock', 'Charlie', 'Cody', 'Ronnie','Matthew']
+var friends = ['Brock','Charlie','Cody','Ronnie','Matthew']
 for(x = 0; x<friends.length; x++){    
     for (i = 99; i > 1; i--){
     
-        console.log( [i] + ' ' + 'lines of code in the file,' + ' ' + [i] + ' ' + 'lines of code;' + ' ' + friends[x] + ' ' + 'strikes one out, clears it all out,' );       
+        console.log( [i] + ' ' + 'lines of code in the file,' + ' ' + [i] + ' ' + 'lines of code;' + ' ' + friends[x] + ' ' + 'strikes one out, clears it all out,' + ' ' + [i-1] + ' ' + 'lines of code in the file'  );       
     }
 console.log([1] + ' ' + 'line of code in the file,' + ' ' + [1] + ' ' + 'line of code;' + ' ' + friends[x] + ' ' + 'strikes one out, clears it all out, no more lines of code in the file.')
 }
